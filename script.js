07bdd3b93c002ebf93b78f5797caa81420b1e884
Your Name <new.anaspakattippara@gmail.com>
2024-02-19 10:30:02 +0530

    ashkar

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const person = {
-    profile_pic: "./image/faheem.jpg",
-    profile_logo: "./image/DBI.jpg",
+    profile_pic: "./image/ASHKARK.jpeg",
+    profile_logo: "",
 
-    Name: "Faheem Ahmad",
-    designation: "Company Owner ",
-    des_link_text: "@National Point",
+    Name: "Askar Kallooravi",
+    designation: "Marketing Specialist ",
+    des_link_text: "",
     des_link: "",
-    contact: "./contact/faheem.vcf",
+    contact: "./contact/ASHKAR.vcf",
 };
 
 const contact ={
-    contact_no: "+971555702102",
-    whatsapp: "971555702102",
-    location: "https://www.google.com/maps/place/25%C2%B016'33.5%22N+55%C2%B018'33.6%22E/@25.2759581,55.3093394,15z/data=!4m4!3m3!8m2!3d25.2759581!4d55.3093394",
-    email: "faheemahmad982@gmail.com",
+    contact_no: "+971588847056",
+    whatsapp: "971588847056",
+    location: "https://maps.app.goo.gl/kVJCAxMASvxZTYmH6?g_st=ic",
+    email: "askuashkar484@gmail.com",
 };
 
 const profilelogo=[
@@ -26,12 +26,12 @@ const profilelogo=[
 
 const links =[
     facebook={
-    visibil: true,
+    visibil: false,
     link:"https://m.facebook.com/people/National-Point/61555328157061/"
   },
     instagram={
     visibil: true,
-    link:"https://www.instagram.com/nationalpoint7/?igsh=MTZvNGIzajZrMnQ2bw%3D%3D&amp;utm_source=qr"
+    link:"https://www.instagram.com/___as.hku?igsh=cmkzaXBwb20yZXZ0&amp;utm_source=qr"
   },
     skype={
     visibil: false,
@@ -55,10 +55,10 @@ const links =[
   },
     mail={
     visibil: true,
-    link:"nationalpoint7@gmail.com"
+    link:"askuashkar484@gmail.com"
   },
     google={
-    visibil: true,
+    visibil: false,
     link:"https://g.co/kgs/iYGMWK3"
   },
     telegram={
